96340db93f22f62f6219d4dfc68607905f34c96a
Reynaldi <149289226+ReynaldiAl-fatih@users.noreply.github.com>
2023-10-31 13:42:58 +0700

    Add files via upload

--- proses6v1.php
+++ proses6v1.php
@@ -33,4 +33,5 @@ $pend= $_POST['pendidikan'];
     </tr>
   </tbody>
 </table>
+<a href="latihan6v1.php">Back to home<a>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
